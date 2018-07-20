e5ea842db6d57747d281cb00fe3142d88aac6e1c
prashant-webkul <prashant.singh852@webkul.com>
2018-07-20 20:37:16 +0530

    Mass actions working and created structure for customer package and removed some bugs from data grid and home page and made layouts for sign in and sign up package of customers and made the customer guard for customer also with model named customer

--- src/Resources/views/layouts/master.blade.php
+++ src/Resources/views/layouts/master.blade.php
@@ -8,7 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('vendor/webkul/shop/assets/css/shop.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/webkul/ui/assets/css/ui.css') }}"> @yield('head') @yield('css')
+    <link rel="stylesheet" href="{{ asset('vendor/webkul/ui/assets/css/ui.css') }}">
+    @yield('head')
+    @yield('css')
 
 </head>
 
