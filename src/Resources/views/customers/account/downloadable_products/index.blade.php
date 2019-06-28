6f194e2538abddd2c1e5cea9206bd7f1d2b4021e
jitendra <jitendra@webkul.com>
2019-06-28 19:48:52 +0530

    updated

--- src/Resources/views/customers/account/downloadable_products/index.blade.php
+++ src/Resources/views/customers/account/downloadable_products/index.blade.php
@@ -1,7 +1,7 @@
 @extends('shop::layouts.master')
 
 @section('page_title')
-    {{ __('shop::app.customer.account.order.index.page-title') }}
+    {{ __('shop::app.customer.account.downloadable_products.title') }}
 @endsection
 
 @section('content-wrapper')
@@ -14,23 +14,23 @@
             <div class="account-head mb-10">
                 <span class="back-icon"><a href="{{ route('customer.account.index') }}"><i class="icon icon-menu-back"></i></a></span>
                 <span class="account-heading">
-                    {{ __('shop::app.customer.account.order.index.title') }}
+                    {{ __('shop::app.customer.account.downloadable_products.title') }}
                 </span>
 
                 <div class="horizontal-rule"></div>
             </div>
 
-            {!! view_render_event('bagisto.shop.customers.account.orders.list.before') !!}
+            {!! view_render_event('bagisto.shop.customers.account.downloadable_products.list.before') !!}
 
             <div class="account-items-list">
                 <div class="account-table-content">
 
-                    {!! app('Webkul\Shop\DataGrids\OrderDataGrid')->render() !!}
+                    {!! app('Webkul\Shop\DataGrids\DownloadableProductDataGrid')->render() !!}
                     
                 </div>
             </div>
 
-            {!! view_render_event('bagisto.shop.customers.account.orders.list.after') !!}
+            {!! view_render_event('bagisto.shop.customers.account.downloadable_products.list.after') !!}
 
         </div>
 
